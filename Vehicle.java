6d1124e57e023bcf1f265f6ffb621e8b101f0d60
jrriahee <jrriahee@gmail.com>
2025-10-15 22:41:02 +0600

    Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,9 @@
+public interface Vehicle {
+    void setMake(String make);
+    void setModel(String model);
+    void setYear(int year);
+
+    String getMake();
+    String getModel();
+    int getYear();
+}
