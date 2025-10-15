ba738ce21d2567aa385b82bf43efc64eca08be52
jrriahee <jrriahee@gmail.com>
2025-10-15 22:59:20 +0600

    TruckVehicle.java

---
+++ TruckVehicle.java
@@ -0,0 +1,7 @@
+public interface TruckVehicle {
+    void setCargoCapacity(double tons);
+    void setTransmissionType(String transmission);
+
+    double getCargoCapacity();
+    String getTransmissionType();
+}
